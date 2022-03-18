70993153084086f3a25ea81b76b60778eb0bb67a
Irving Juárez <irvingjuarez274@gmail.com>
2022-03-18 12:50:33 -0600

    The first maquetation of the Home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,23 @@
 const Home: React.FC = (): JSX.Element => {
   return(
-    <h2>I am the HOME page</h2>
+    <article className="home">
+      <section className="home__content">
+        <span className="home__subtitle">Hi there! My name is</span>
+
+        <h2 className="home__name">Irving Juárez</h2>
+
+        <h2 className="home__brief">I build stuff for the web</h2>
+
+        <h3 className="home__description">
+          I am a Front-end Engineer focused on UI design and user experience. Currently looking for a job.
+        </h3>
+
+        <div>
+          <button>View my work.</button>
+          <button>Contact me.</button>
+        </div>
+      </section>
+    </article>
   )
 }
 
