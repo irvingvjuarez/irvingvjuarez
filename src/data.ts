5a009d00af5c446d68a2b9560b713f3efcc3f190
Irving Juárez <irvingjuarez274@gmail.com>
2022-03-26 16:20:34 -0600

    Update of the data

--- src/data.ts
+++ src/data.ts
@@ -4,6 +4,50 @@ const data = {
   github: "https://github.com/irvingvjuarez",
   email: "irvingjuarez274@gmail.com",
   currentCompany: "Lyfbridge",
+  reposUrl: "https://github.com/irvingvjuarez?tab=repositories",
+  projects: [
+    {
+      id: 0,
+      status: "Personal Project",
+      title: "Yard Sale",
+      achievements: [
+        "Build and deploy a SPA using React.js and TypeScript",
+        "Consuming and printing data from fakestore API",
+        "Dynamic routing",
+        "Implementation of SEO increasing site visibility by 50%"
+      ],
+      technologies: ["TypeScript", "SCSS", "HTML", "React.js"],
+      repo: "https://github.com/irvingvjuarez/yard-sale",
+      online: "https://shopping-cart-juarez.netlify.app/",
+    },
+    {
+      id: 1,
+      status: "Featured Project",
+      title: "Get Hired Code Tracker",
+      achievements: [
+        "Leading the UI team and contributing the creation of the design system",
+        "Presenting the project to an audience of +10,000 non-technical people",
+        "Working with vscode API using TypeScript and Next.js to build and deploy a plugin in the Microsoft Marketplace",
+        "User authentication with three social media platforms using Auth0"
+      ],
+      technologies: ["Next.js", "TypeScript", "VSCode API", "Auth0"],
+      repo: "https://github.com/Platzi-Master-C8/VSC-Plugins",
+      online: "https://marketplace.visualstudio.com/items?itemName=GetHiredCodeTracker.gethired-code-tracker",
+    },
+    {
+      id: 2,
+      status: "Personal Project",
+      title: "Weather App",
+      achievements: [
+        "Build and deploy a SPA using React.js",
+        "Consuming and printing data from a weather API",
+        "Implementation of a dynamic map with +3 layers of visibility",
+      ],
+      technologies: ["JavaScript", "React.js", "SASS", "HTML"],
+      repo: "https://github.com/irvingvjuarez/weatherApp",
+      online: "https://irvingjuarezweatherapp.netlify.app/",
+    }
+  ],
   repos: [
     {
       id: 0,
@@ -11,7 +55,7 @@ const data = {
       repo: "https://github.com/irvingvjuarez/yard-sale",
       online: "https://shopping-cart-juarez.netlify.app/",
       description: "Yard Sale emulates an e-commerce app flow. This project was based on React Practical course on Platzi and was created to simulate an shopping cart.",
-      technologies: ["TypeScript", "SCSS", "HTML", "React.js"]
+      technologies: ["React.js", "SASS", "HTML", "Fakestore API", "TypeScript"]
     },
     {
       id: 1,
@@ -27,7 +71,7 @@ const data = {
       repo: "https://github.com/irvingvjuarez/weatherApp",
       online: "https://irvingjuarezweatherapp.netlify.app/",
       description: "Figure out what the weather will be within 24 hrs or for the next 7 days. Enjoy an awesome UI while checking the time out.",
-      technologies: ["JavaScript", "SCSS", "HTML", "React.js"]
+      technologies: ["JavaScript", "Weather API", "SCSS", "HTML", "React.js"]
     },
     {
       id: 3,
@@ -35,7 +79,7 @@ const data = {
       repo: "https://github.com/irvingvjuarez/qwerty-keyboard",
       online: "https://irvingjuarez.github.io/qwerty-keyboard/",
       description: "Algorithmia challenge. Enter an input and the keyboard will emulate the typing in the screen. DOM manipulation in action for this app.",
-      technologies: ["JavaScript", "CSS", "HTML"]
+      technologies: ["JavaScript", "CSS", "HTML", "Dynamic Programming"]
     },
     {
       id: 4,
@@ -51,7 +95,7 @@ const data = {
       repo: "https://github.com/irvingvjuarez/GraphiCal",
       online: "https://graphical.netlify.app/",
       description: "Graphing Calculator used to print dots in the canvas.",
-      technologies: ["JavaScript", "SCSS", "HTML", "React.js"]
+      technologies: ["JavaScript", "SCSS", "HTML", "React.js", "Canvas"]
     }
   ]
 }
